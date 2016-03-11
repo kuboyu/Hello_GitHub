33f2de2fdb18dee3b5b45ec1c8688d53e96f8f81
kuboyu <yuki330beat@gmail.com>
2016-03-12 02:12:46 +0900

    Delete hello.java

--- hello.java
+++
@@ -1,9 +0,0 @@
-package jp.co.hello;
-
-//GitHub練習
-public class hello {
-	public static void main	(String[] args)	{
-	  System.out.println("hello wold!");
-	  System.out.println("hello GitHub!");
-	}
-}
