6e74d7d70d42d1fbb0fb81410202b35a8448ed38
kuboyu <yuki330beat@gmail.com>
2016-03-12 02:12:16 +0900

    First Commit!

---
+++ hello.java
@@ -0,0 +1,9 @@
+package jp.co.hello;
+
+//GitHub練習
+public class hello {
+	public static void main	(String[] args)	{
+	  System.out.println("hello wold!");
+	  System.out.println("hello GitHub!");
+	}
+}
